f04e09dc9add0c1c6433624495fac76f068fb9e6
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2026-01-07 22:01:18 +0100

    Update variable.tf

--- eks-terraform/variable.tf
+++ eks-terraform/variable.tf
@@ -1,3 +1,9 @@
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+  default     = "project-node-group"
+}
+
 variable "cluster_role_arn" {
   description = "Existing IAM Role ARN for EKS Cluster"
   type        = string

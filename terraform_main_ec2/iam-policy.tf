14a1f345372fac892761f65cb214d50d73b2b8b2
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2026-01-07 19:24:11 +0100

    Update iam-policy.tf

--- terraform_main_ec2/iam-policy.tf
+++ terraform_main_ec2/iam-policy.tf
@@ -1,51 +1,24 @@
-resource "aws_iam_policy" "eks_policy" {
-  name        = "eks_policy"
-  description = "Custom policy for EKS full access"
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "eks:*",
-            "Resource": "*"
-        }
-    ]
-}
-EOF
-}
+# في Learner Lab لا يمكن إنشاء IAM Policy جديدة
+# لذلك نحذف/نعلّق مورد aws_iam_policy ونستخدم السياسات الجاهزة فقط
 
-resource "aws_iam_role_policy_attachment" "eks_policy_attachment" {
-  role       = aws_iam_role.iam-role.name
-  policy_arn = aws_iam_policy.eks_policy.arn
-}
-resource "aws_iam_role_policy_attachment" "iam-policy" {
-  role = aws_iam_role.iam-role.name
-  # Just for testing purpose, don't try to give administrator access in production
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_full_access" {
-  role       = aws_iam_role.iam-role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
+# ربط الدور الافتراضي LabRole بالسياسات الأساسية المطلوبة
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.iam-role.name
+  role       = "LabRole"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.iam-role.name
+  role       = "LabRole"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "cloudformation_full_access" {
-  role       = aws_iam_role.iam-role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCloudFormationFullAccess"
+resource "aws_iam_role_policy_attachment" "ec2_full_access" {
+  role       = "LabRole"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "iam_full_access" {
-  role       = aws_iam_role.iam-role.name
-  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+resource "aws_iam_role_policy_attachment" "cloudformation_full_access" {
+  role       = "LabRole"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCloudFormationFullAccess"
 }

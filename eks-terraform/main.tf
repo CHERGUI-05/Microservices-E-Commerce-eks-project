e421328600cd94f6fff2b238f2b967fd6f775b4e
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2026-01-07 21:53:19 +0100

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -1,17 +1,14 @@
-# اسم مجموعة الـ Nodes في الـ EKS
 variable "node_group_name" {
   description = "Name of the EKS node group"
   type        = string
   default     = "project-node-group"
 }
 
-# الـ ARN للـ IAM Role الخاص بالـ Cluster (موجود مسبقًا)
 variable "cluster_role_arn" {
   description = "Existing IAM Role ARN for EKS Cluster"
   type        = string
 }
 
-# الـ ARN للـ IAM Role الخاص بالـ Worker Nodes (موجود مسبقًا)
 variable "worker_role_arn" {
   description = "Existing IAM Role ARN for EKS Worker Nodes"
   type        = string

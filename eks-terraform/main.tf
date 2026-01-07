10d95e0c75f59d0882a2925f0ec2cf1e8e441c63
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2026-01-07 21:22:33 +0100

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -6,11 +6,20 @@ provider "aws" {
 # Terraform Backend (S3)
 # ----------------------------
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.25.0"
+    }
+  }
+
   backend "s3" {
     bucket = "riheb-eks-terraform-state"   # غيّري الاسم لاسم فريد خاص بك
-    key    = "eks/terraform.tfstate"
+    key    = "eks/terraform.tfstate"       # مسار تخزين الـ state
     region = "us-east-1"
   }
+
+  required_version = ">= 1.6.3"
 }
 
 # ----------------------------

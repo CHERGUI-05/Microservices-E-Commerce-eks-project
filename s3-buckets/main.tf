feb55ab209e2884c44cdb5e1689895c9fdd2723d
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2026-01-07 18:57:46 +0100

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -2,11 +2,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# توليد لاحقة عشوائية لضمان أسماء فريدة
+resource "random_id" "suffix1" {
+  byte_length = 4
+}
+
+resource "random_id" "suffix2" {
+  byte_length = 4
+}
+
+# Bucket 1 مع اسم عشوائي
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "aluruarumullaa1"
+  bucket = "riheb-eks-bucket1-${random_id.suffix1.hex}"
 
   tags = {
-    Name        = "aluruarumullaa1"
+    Name        = "riheb-eks-bucket1-${random_id.suffix1.hex}"
     Environment = "dev"
   }
 }
@@ -18,11 +28,12 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
   }
 }
 
+# Bucket 2 مع اسم عشوائي
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "arumullaaluruu1"
+  bucket = "riheb-eks-bucket2-${random_id.suffix2.hex}"
 
   tags = {
-    Name        = "arumullaaluruu1"
+    Name        = "riheb-eks-bucket2-${random_id.suffix2.hex}"
     Environment = "dev"
   }
 }

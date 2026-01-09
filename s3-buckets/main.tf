662665f9d1c1bfb4976ca6802848e4c27652192c
CHERGUI-05 <riheb.chergui@univ-constantine2.dz>
2026-01-09 08:33:46 +0100

    Update main.tf

--- s3-buckets/main.tf
+++ s3-buckets/main.tf
@@ -12,7 +12,7 @@ resource "random_id" "suffix2" {
 }
 
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "riheb-eks-bucket1-${random_id.suffix1.hex}"
+  bucket = "rihab-eks-bucket1-${random_id.suffix1.hex}"
 
   tags = {
     Name        = "riheb-eks-bucket1-${random_id.suffix1.hex}"
@@ -28,7 +28,7 @@ resource "aws_s3_bucket_versioning" "bucket1_versioning" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "riheb-eks-bucket2-${random_id.suffix2.hex}"
+  bucket = "rihab-eks-bucket2-${random_id.suffix2.hex}"
 
   tags = {
     Name        = "riheb-eks-bucket2-${random_id.suffix2.hex}"
